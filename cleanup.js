50e54e765f839ff81f26f23458739c0594878560
SK_Artemis <98838698+SK-24Artemis@users.noreply.github.com>
2023-12-08 13:57:44 +0100

    Update cleanup.js
    have to update cleanup.js myself since an update broke it

--- cleanup.js
+++ cleanup.js
@@ -1,5 +1,17 @@
-/** @param {NS} ns **/
+/**SK_ Edit
+ * WARNING: This script will delete all our scripts and text files.  Make sure
+ * the files are backed up somewhere.
+ *
+ * Remove all our scripts and text files from the home server, except for this
+ * script.  If a script is running, it will not be removed.
+ *
+ * Usage: run cleanup.js
+ *
+ * @param ns The Netscript API.
+ */
 export async function main(ns) {
-    for (let file of ns.ls('home', 'Temp/'))
-        ns.print((ns.rm(file) ? "Removed " : "Failed to remove ") + file);
+    const suffix = [".js", ".script", ".ns", ".txt"];
+    const rm_file = (file) => ns.rm(file);
+    const rm_all_files = (pattern) => ns.ls("home", pattern).forEach(rm_file);
+    suffix.forEach(rm_all_files);
 }
