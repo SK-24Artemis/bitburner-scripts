562118cd08332e0465c9355c7af9d2135b99a461
SK_Artemis <98838698+SK-24Artemis@users.noreply.github.com>
2023-12-10 19:11:06 +0100

    Create clean1.js (deletes all)

---
+++ clean1.js
@@ -0,0 +1,17 @@
+/**SK_ Edit
+ * WARNING: This script will delete all our scripts and text files.  Make sure
+ * the files are backed up somewhere.
+ *
+ * Remove all our scripts and text files from the home server, except for this
+ * script.  If a script is running, it will not be removed.
+ *
+ * Usage: run clean1.js
+ *
+ * @param ns The Netscript API.
+ */
+export async function main(ns) {
+    const suffix = [".js", ".script", ".ns", ".txt"];
+    const rm_file = (file) => ns.rm(file);
+    const rm_all_files = (pattern) => ns.ls("home", pattern).forEach(rm_file);
+    suffix.forEach(rm_all_files);
+}
