9084af4c88a6ce601c88c3c1bd66b979218e338e
SK_Artemis <98838698+SK-24Artemis@users.noreply.github.com>
2023-12-10 19:09:34 +0100

    Update cleanup.js

--- cleanup.js
+++ cleanup.js
@@ -1,17 +1,5 @@
-/**SK_ Edit
- * WARNING: This script will delete all our scripts and text files.  Make sure
- * the files are backed up somewhere.
- *
- * Remove all our scripts and text files from the home server, except for this
- * script.  If a script is running, it will not be removed.
- *
- * Usage: run cleanup.js
- *
- * @param ns The Netscript API.
- */
+/** @param {NS} ns **/
 export async function main(ns) {
-    const suffix = [".js", ".script", ".ns", ".txt"];
-    const rm_file = (file) => ns.rm(file);
-    const rm_all_files = (pattern) => ns.ls("home", pattern).forEach(rm_file);
-    suffix.forEach(rm_all_files);
+    for (let file of ns.ls('home', 'Temp/'))
+        ns.print((ns.rm(file) ? "Removed " : "Failed to remove ") + file);
 }
