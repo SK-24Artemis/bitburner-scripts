8e5306d0a2d180f6b5db76f68fe3323abed07b77
SK_Artemis <98838698+SK-24Artemis@users.noreply.github.com>
2023-12-24 07:46:42 +0100

    Create image1.js

---
+++ image1.js
@@ -0,0 +1,8 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  ns.tprintRaw(
+    React.createElement(`img`, {
+      src: `https://cdn.discordapp.com/attachments/928919860891750410/1187806578116661328/a_winner_is_you20110724-22047-1nd3wif.jpg?ex=65983a14&is=6585c514&hm=bc3f632c4c60390a24dfc00e5fbab8f1c7110bb9c2ec91de367cb65939b15201&`
+    })
+  );
+}
